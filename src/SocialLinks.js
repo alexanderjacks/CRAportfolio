e25419e5a3bd4e2197320a90fc9ad58411a06b86
alexanderjacks <alexanderthejacks@gmail.com>
2018-11-23 16:11:41 -0800

    :memo: adds resume

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -4,6 +4,7 @@ class SocialLinks extends Component {
 	render() {
 		return(
 			<div className="d-flex justify-content-around">
+				<a href="https://github.com/alexanderjacks/CRAportfolio/blob/master/public/_dec18_resume.pdf"  target="_blank" rel="noopener noreferrer"><i class="far fa-file-pdf"></i></a>
 				<a href="https://www.instagram.com/a.d.jacks/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
 				<a href="https://www.linkedin.com/in/alexander-jacks/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
 				<a href="https://github.com/alexanderjacks" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
